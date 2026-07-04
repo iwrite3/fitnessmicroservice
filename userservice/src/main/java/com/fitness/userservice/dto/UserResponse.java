9c22fc2ecd964c4bd3ebfc658f76240869878492
icode <140626465+iwrite3@users.noreply.github.com>
2026-07-04 22:24:32 +0530

    Update UserResponse.java

--- userservice/src/main/java/com/fitness/userservice/dto/UserResponse.java
+++ userservice/src/main/java/com/fitness/userservice/dto/UserResponse.java
@@ -6,7 +6,7 @@ import java.time.LocalDateTime;
 @Data
 public class UserResponse {
         private String id;
-        private String keycloakId;
+        private String firebaseId;
         private String email;
         private String password;
         private String firstName;
