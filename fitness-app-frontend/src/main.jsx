1d29b1c8974b13162c9766c8fb4e20943e2c9642
icode <140626465+iwrite3@users.noreply.github.com>
2026-07-05 22:07:10 +0530

    Update main.jsx

--- fitness-app-frontend/src/main.jsx
+++ fitness-app-frontend/src/main.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import{ authConfig } from './authConfig'
 import { Provider } from 'react-redux'
-import {store} from './store/store'
+import { store } from './store/store'
 import App from './App'
-import { AuthProvider } from 'react-oauth2-code-pkce'
 
-// As of React 18
+// Removed AuthProvider and authConfig imports completely.
+// Firebase doesn't need a wrapper here because we handle it inside App.jsx!
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <AuthProvider authConfig={authConfig}>
   <Provider store={store}>
     <App />
-  </Provider>,
-  </AuthProvider>,
+  </Provider>
 )
